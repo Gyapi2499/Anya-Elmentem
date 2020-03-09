e6cdea473b118a7d7507826126eb3ebc8638f023
boritek22 <53436656+boritek22@users.noreply.github.com>
2020-03-09 17:07:17 +0100

    UserController és AuthenticatedUser
    Az UserController alapmetódusai és az AuthenticatedUser létrehozása.

--- anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/UserController.java
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/UserController.java
@@ -1,22 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.elte.anyaelmentem.controllers;
 
+import hu.elte.anyaelmentem.entities.Users;
+import hu.elte.anyaelmentem.repositories.UserRepository;
+import hu.elte.anyaelmentem.security.AuthenticatedUser;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-/**
- *
- * @author Máté
- */
 
 @CrossOrigin
 @RestController
 @RequestMapping("/users")
 public class UserController {
+     @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private AuthenticatedUser authenticatedUser;
+
+    @PostMapping("/users/register")
+    public ResponseEntity<Users> register(@RequestBody Users user) {
+        Optional<Users> oUser = userRepository.findByEmail(user.getEmail());
+        if (oUser.isPresent()) {
+            return ResponseEntity.badRequest().build();
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Users.Role.USER);
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<Users> login() {
+        return ResponseEntity.ok(authenticatedUser.getUser());
+    }
+
+    @GetMapping("/users")
+    public List<Users> getUsers() {
+        return userRepository.findAll();
+    }
 }
