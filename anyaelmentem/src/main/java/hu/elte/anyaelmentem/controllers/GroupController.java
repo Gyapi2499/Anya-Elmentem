d47cf931e9f8ab7d0eb1590a620cdc1b2a059410
boritek22 <53436656+boritek22@users.noreply.github.com>
2020-03-16 20:14:53 +0100

    Delete Group és Member
    DeleteGroup és DeleteMember metódusok létrehozása. Csak admin által végzett műveletek konkretizálása.
    
    Co-Authored-By: macskovity <macskovity@users.noreply.github.com>
    Co-Authored-By: dori98 <dori98@users.noreply.github.com>
    Co-Authored-By: gyapi2499 <gyapi2499@users.noreply.github.com>

--- anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,28 +49,51 @@ public class GroupController {
    
    @PostMapping("/addMember")//csapathoz adok hozzá
    public ResponseEntity<Group> addMember(int id, Users addM){
+       if(authenticatedUser.getUser().getRole()== Users.Role.ADMIN || groupRepository.findById(id).get().getAdmins().contains(authenticatedUser.getUser())) {
        Group temp=groupRepository.findById(id).get();
        temp.getUsers().add(addM);
        return ResponseEntity.ok(groupRepository.save(temp));       
-   }
+   } return ResponseEntity.badRequest().build(); 
+}
    
    @PostMapping("/shareAdmin")
    public ResponseEntity<Group> shareAdmin(int id, List<Users> user){
+       if(authenticatedUser.getUser().getRole()== Users.Role.ADMIN || groupRepository.findById(id).get().getAdmins().contains(authenticatedUser.getUser())) {
        Group temp=groupRepository.findById(id).get();
        for(Users u:user){
        temp.getUsers().remove(u);
        temp.getAdmins().add(u);
        }
        return ResponseEntity.ok(groupRepository.save(temp));
-   }
+   } return ResponseEntity.badRequest().build(); 
+}
+   
     @PostMapping("/takeAdmin")
     public ResponseEntity<Group> takeAdmin(int id, List<Users> user){
+       if(authenticatedUser.getUser().getRole()== Users.Role.ADMIN || groupRepository.findById(id).get().getAdmins().contains(authenticatedUser.getUser())) {
        Group temp=groupRepository.findById(id).get();
        for(Users u:user){
        temp.getUsers().add(u);
        temp.getAdmins().remove(u);
        }
        return ResponseEntity.ok(groupRepository.save(temp));
-    }  
+   } return ResponseEntity.badRequest().build(); 
+}
     
+    @DeleteMapping("/deleteGroup/{id}")
+    public ResponseEntity<Void> deleteGroup(@PathVariable int id){
+        if(authenticatedUser.getUser().getRole()== Users.Role.ADMIN || groupRepository.findById(id).get().getAdmins().contains(authenticatedUser.getUser())) {
+        groupRepository.delete(groupRepository.findById(id).get());
+        return ResponseEntity.ok().build();
+       } return ResponseEntity.badRequest().build(); 
+    }
+    
+    @PostMapping("/deleteMember")
+    public ResponseEntity<Group> deleteMember(int id, Users deleteM){
+       if(authenticatedUser.getUser().getRole()== Users.Role.ADMIN || groupRepository.findById(id).get().getAdmins().contains(authenticatedUser.getUser())) {
+       Group temp=groupRepository.findById(id).get();
+       temp.getUsers().remove(deleteM);
+       return ResponseEntity.ok(groupRepository.save(temp));     
+       } return ResponseEntity.badRequest().build(); 
+    }
 }
