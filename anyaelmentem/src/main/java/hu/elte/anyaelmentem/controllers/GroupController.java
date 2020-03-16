d6a6418c08a13a6274141c3b4fe157532c4b1081
dori98 <stefandora98@gmail.com>
2020-03-16 19:14:04 +0100

    GroupController
    A GroupController létrehozása
    
    Co-Authored-By: boritek22 <boritek22@users.noreply.github.com>
    Co-Authored-By: gyapi2499 <gyapi2499@users.noreply.github.com>
    Co-Authored-By: macskovity <macskovity@users.noreply.github.com>

--- anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
@@ -5,18 +5,51 @@
  */
 package hu.elte.anyaelmentem.controllers;
 
+import hu.elte.anyaelmentem.entities.Group;
+import hu.elte.anyaelmentem.entities.Users;
+import hu.elte.anyaelmentem.repositories.GroupRepository;
+import hu.elte.anyaelmentem.repositories.UserRepository;
+import hu.elte.anyaelmentem.security.AuthenticatedUser;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Máté
- */
+
 @CrossOrigin
 @RestController
 @RequestMapping("/group")
 public class GroupController {
 
+    @Autowired
+    private AuthenticatedUser authenticatedUser;
+    
+    @Autowired
+    private GroupRepository groupRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+   
+    /**    
+    * uj csapat 
+    */
+   @PostMapping("/newGroup")
+   public ResponseEntity<Group> newGroup(List<Users> myMember){
+       Group nGroup= new Group();
+       nGroup.setUsers(myMember);
+       nGroup.getAdmins().add(authenticatedUser.getUser());
+       return ResponseEntity.ok(groupRepository.save(nGroup));       
+   }
+   
+   @PostMapping("/addMember")//csapathoz adok hozzá
+   public ResponseEntity<Group> addMember(int id, Users addM){
+       Group temp=groupRepository.findById(id).get();
+       temp.getUsers().add(addM);
+       return ResponseEntity.ok(groupRepository.save(temp));       
+   }
 
 }
