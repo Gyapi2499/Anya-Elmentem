68cc9aac6055a6e3554f878f7acce2a95b6ceb0d
boritek22 <53436656+boritek22@users.noreply.github.com>
2020-03-16 19:41:28 +0100

    Admin jogok
    Admin jogokkal való ellátás és admin jogok elvétele.
    
    Co-Authored-By: macskovity <macskovity@users.noreply.github.com>
    Co-Authored-By: dori98 <dori98@users.noreply.github.com>
    Co-Authored-By: gyapi2499 <gyapi2499@users.noreply.github.com>

--- anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +52,24 @@ public class GroupController {
        temp.getUsers().add(addM);
        return ResponseEntity.ok(groupRepository.save(temp));       
    }
-
+   
+   @PostMapping("/shareAdmin")
+   public ResponseEntity<Group> shareAdmin(int id, List<Users> user){
+       Group temp=groupRepository.findById(id).get();
+       for(Users u:user){
+       temp.getUsers().remove(u);
+       temp.getAdmins().add(u);
+       }
+       return ResponseEntity.ok(groupRepository.save(temp));
+   }
+    @PostMapping("/takeAdmin")
+    public ResponseEntity<Group> takeAdmin(int id, List<Users> user){
+       Group temp=groupRepository.findById(id).get();
+       for(Users u:user){
+       temp.getUsers().add(u);
+       temp.getAdmins().remove(u);
+       }
+       return ResponseEntity.ok(groupRepository.save(temp));
+    }  
+    
 }
