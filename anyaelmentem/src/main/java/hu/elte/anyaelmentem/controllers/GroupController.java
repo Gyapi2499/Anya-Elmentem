c448672aa943a87b99a2bc97bd6c15569fbe3e61
Macskovity <ma.matyi@gmail.com>
2020-03-16 18:04:23 +0100

    Chores Group controllerek és chores repository létrehozása

---
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/GroupController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.elte.anyaelmentem.controllers;
+
+import hu.elte.anyaelmentem.entities.ToDos;
+import hu.elte.anyaelmentem.entities.Users;
+import hu.elte.anyaelmentem.repositories.GroupRepository;
+import hu.elte.anyaelmentem.repositories.ToDoRepository;
+import hu.elte.anyaelmentem.repositories.UserRepository;
+import hu.elte.anyaelmentem.security.AuthenticatedUser;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Máté
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/group")
+public class GroupController {
+
+
+}
