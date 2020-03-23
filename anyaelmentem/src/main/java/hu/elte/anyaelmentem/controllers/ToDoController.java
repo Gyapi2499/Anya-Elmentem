6b050682a9a2c0111d5b928bdef8ad32053e2afe
dori98 <stefandora98@gmail.com>
2020-03-23 20:26:04 +0100

    heti feladatok lekérdezése -ToDoController
    a különböző userek saját heti feladatainak lekérdezése

--- anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/ToDoController.java
+++ anyaelmentem/src/main/java/hu/elte/anyaelmentem/controllers/ToDoController.java
@@ -6,11 +6,13 @@
 package hu.elte.anyaelmentem.controllers;
 
 import hu.elte.anyaelmentem.entities.ToDo;
-import hu.elte.anyaelmentem.entities.User;
 import hu.elte.anyaelmentem.repositories.GroupRepository;
 import hu.elte.anyaelmentem.repositories.ToDoRepository;
 import hu.elte.anyaelmentem.repositories.UserRepository;
 import hu.elte.anyaelmentem.security.AuthenticatedUser;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Máté
- */
+
 @CrossOrigin
 @RestController
 @RequestMapping("/todo")
@@ -44,5 +41,22 @@ public class ToDoController {
     @Autowired
     private UserRepository userRepository;    
     
+    @GetMapping("/getWeek/{userId}")
+    public ResponseEntity<List<ToDo>> getWeek(@PathVariable String userId){
+        LocalDateTime now = LocalDateTime.now();
+        DayOfWeek day = now.getDayOfWeek();
+        long dayNum = day.getLong(ChronoField.DAY_OF_WEEK);
+        LocalDateTime monday = now.withDayOfYear(now.getDayOfYear()-(int)dayNum-1).withHour(0).withMinute(0).withSecond(0);
+        LocalDateTime nextMonday = now.withDayOfYear(now.getDayOfYear()-(int)dayNum+7).withHour(0).withMinute(0).withSecond(0);
+        
+        List<ToDo> weekToDo= new ArrayList<ToDo>();
+                
+        for(ToDo t : toDoRepository.findAllByUserId(userId).get()){
+            if(t.getToDate().compareTo(monday)>0 && t.getToDate().compareTo(nextMonday)<0 ){
+                weekToDo.add(t);                
+            }
+        } 
+        return ResponseEntity.ok(weekToDo);       
+    }
 
 }
