6d1b3017ad583586665ab2e84579414f31623951
Gyapi2499 <g70r8p@inf.elte.hu>
2020-04-10 11:03:37 +0200

    test update

--- anyaelmentem/src/test/java/hu/elte/anyaelmentem/UserControllerTest.java
+++ anyaelmentem/src/test/java/hu/elte/anyaelmentem/UserControllerTest.java
@@ -6,30 +6,30 @@
 package hu.elte.anyaelmentem;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hu.elte.anyaelmentem.controllers.ChoreController;
-import hu.elte.anyaelmentem.controllers.UserController;
-import hu.elte.anyaelmentem.entities.User;
+import hu.elte.anyaelmentem.entities.Chore;
 import hu.elte.anyaelmentem.repositories.ChoreRepository;
 import hu.elte.anyaelmentem.repositories.GroupRepository;
 import hu.elte.anyaelmentem.repositories.ToDoRepository;
 import hu.elte.anyaelmentem.repositories.UserRepository;
 import hu.elte.anyaelmentem.security.AuthenticatedUser;
 import java.util.Base64;
+import static org.assertj.core.api.Java6Assertions.*;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.springframework.util.MultiValueMap;
 
 /**
  *
@@ -89,4 +89,25 @@ public class UserControllerTest {
             mockMvc.perform(get("/chores/allChores").header("Access-Control-Allow-Origin", "*").header("Authorization" , "Basic "+encoded).contentType("application/json")).andExpect(status().is(401));
         }
     }
+    
+    /*@Test
+    void whenValidInput_thenReturnsUserResource() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/chores/newChore/asd").contentType("application/json")).andReturn();
+        System.out.println( mvcResult.getResponse().getContentAsString());
+        Chore chore = new Chore("asd");
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+
+        assertThat(objectMapper.writeValueAsString(chore)).isEqualToIgnoringWhitespace(actualResponseBody);
+    }*/
+    
+    @Test
+    void whenValidInput_thenMapsToBusinessModel() throws Exception {
+      Chore chore = new Chore("asd");
+      mockMvc.perform(post("/chores/newChore/asd").contentType("application/json"));
+
+      ArgumentCaptor<Chore> choreCaptor = ArgumentCaptor.forClass(Chore.class);
+      verify(choreRepository, times(1)).save(choreCaptor.capture());
+      assertThat(choreCaptor.getValue().getName()).isEqualTo(chore.getName());
+    }
+
 }
