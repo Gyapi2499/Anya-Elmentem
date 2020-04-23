7228610aaefacf3082880f8a7b403c836e2bec91
dori98 <stefandora98@gmail.com>
2020-04-23 21:46:08 +0200

    NewTodo még mindig nem kész

--- frontend/anya-elmentem/src/views/newTodo/newTodo.vue
+++ frontend/anya-elmentem/src/views/newTodo/newTodo.vue
@@ -3,44 +3,48 @@
     <div >
     <h2>Melyik csoportomnak szerétnék feladatot adni?</h2>
     <br>
-            <select v-model="selected" :options='group' @click="toggle" >
+            <select v-model="selected" :options='group'>
               <option disabled value="" >Please select one</option>
-              <option v-for="i in group" v-bind:key='i' v-bind:value='i'  >{{i.id}}</option>
+              <option v-for="i in group" v-bind:key='i' v-bind:value='i'  >{{i}}</option>
             </select >
+            <br>
     <br>
     </div>
-    <div >
     <h2>Feladat nekem</h2>
-    <form id="nekem" @submit="onSubmit()">
+    <form id="nekem" >
         <a>Feladat megnevezése:</a>
         <br>
-        <input type= "text " name= "title" placeholder="Add todo..."><br>
+        <input v-model="feladat" type= "text " name= "title" placeholder="Add todo..."><br>
         <a> Feladat kezdete:</a>
         <br>
-        <datetime type="datetime" v-model="datetimeTheming" ></datetime>
+        <datetime v-model="kezdet" type="datetime"  format="yyyy-MM-dd HH:mm:ss"></datetime>
         <br>
         <a> Feladat vége:</a>
-        <datetime type="datetime" v-model="datetime13" format="yyyy-MM-dd HH:mm:ss"></datetime>
+        <datetime type="datetime" v-model="veg" format="yyyy-MM-dd HH:mm:ss"></datetime>
         <br>
-        <input type='submit' value='submit'>
+        <button @click="save()"> Mentés </button>
         </form>
-      <form id="masnak" @submit="onSubmit()" >
+    <div >
+      <form id="masnak"  >
         <h2>Feladat másnak:</h2>
         <a>Feladat megnevezése:</a>
         <br>
-        <input type= "text " name= "title" placeholder="Add todo..."> <br>
-        <a> Feladat kezdete:</a> <datetime type="datetime" v-model="datetimeTheming" ></datetime>
+        <input type= "text "  v-model="feladat" name= "title" placeholder="Add todo..."> <br>
+        <a> Feladat kezdete:</a>
         <br>
-        <a> Feladat vége:</a>  <datetime type="datetime" v-model="datetimeTheming" ></datetime>
+        <datetime type="datetime" v-model="kezdet" format="yyyy-MM-dd HH:mm:ss"></datetime>
+        <br>
+        <a> Feladat vége:</a>
+        <datetime type="datetime" v-model="veg" format="yyyy-MM-dd HH:mm:ss"></datetime>
         <br>
         <a> Kit választok:</a>
         <br>
-        <select v-model="selected" :options='cats' >
+        <select v-model="select"  >
           <option disabled value="">Please select one</option>
-          <option v-for="cat in memberList" v-bind:key='cat' v-bind:value='cat'>{{cat.email}}</option>
+          <option v-for="cat in memberList" v-bind:key='cat' v-bind:value='cat'>{{cat.name}}</option>
         </select>
         <br>
-        <input type='submit' value='submit'>
+        <button @click="save2()"> Mentés </button>
     </form>
     </div>
   </div>
@@ -50,20 +54,38 @@
 import { mapActions, mapState } from 'vuex'
 export default {
   name: 'newTodo',
+  data () {
+    return {
+      selected: '',
+      select: '',
+      feladat: '',
+      kezdet: '',
+      veg: ''
+    }
+  },
   methods: {
-    toggle () {
-      this.show = !this.show
-    },
     ...mapActions('newTodo', ['getMemberList', 'addTodo', 'getGroup']),
-    async save () {},
-    async onSubmit () {}
+    save () {
+      this.addTodo({ userId: this.logUser.email, chores: this.feladat, fromDate: this.kezdet, toDate: this.veg, groupId: this.selected, token: this.token })
+    },
+    save2 () {
+      this.addTodo({ userId: this.select.email, chores: this.feladat, fromDate: this.kezdet, toDate: this.veg, groupId: this.selected, token: this.token })
+    }
   },
   created () {
-    this.getmemberList({ user: this.user })
+    this.getGroup({ id: this.logUser.email, token: this.token })
+    this.getMemberList({ id: this.selected, token: this.token })
   },
   computed: {
-    ...mapState(['user', 'Http']),
+    ...mapState(['logUser', 'token']),
     ...mapState('newTodo', ['memberList', 'group'])
+  },
+  watch: {
+    selected () {
+      console.log('sisijsixjknfjhsbghbrfhvbsbdhvbjhsanvjhbfdjgvbhjdbvhwbhvbjhsdbvhbsdbvjsb')
+      console.log(this.selected)
+      this.getMemberList({ id: this.selected, token: this.token })
+    }
   }
 }
 
