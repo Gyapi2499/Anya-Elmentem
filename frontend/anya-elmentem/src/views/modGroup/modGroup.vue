e68791fc786dbb26f1c8a6df04bcd5b5ea3d8b5f
boritek22 <53436656+boritek22@users.noreply.github.com>
2020-04-25 01:07:09 +0200

    modGroup metódusainak "feltöltése"
    modGroup metódusainak "feltöltése" valós funkciókkal

--- frontend/anya-elmentem/src/views/modGroup/modGroup.vue
+++ frontend/anya-elmentem/src/views/modGroup/modGroup.vue
@@ -5,34 +5,52 @@
   <br>
   <h2>Melyik csoportodat szeretnéd módosítani?</h2>
     <select v-model="selected" :options='group' >
-      <option disabled value="">Válaszd ki:</option>
+      <option disabled value="">Válaszd ki a csoportot:</option>
       <option v-for="i in group" v-bind:key='i' v-bind:value='i'  >{{i}}</option>
     </select>
     <br>
 
     <h2>Adj hozzá egy új csoporttagot!</h2>
     <br>
-    <select name="" id="" multiple>
-      <option disabled value="">Válaszd ki, kit akarsz hozzáadni a csoportodhoz!</option>
-      <option>...</option>
-      <option><button>Add</button></option>
-      </select>
-      <br>
+    <select v-model="selected" multiple>
+      <option disabled value="" >Válaszd ki, kiket akarsz hozzáadni a csoportodhoz!</option>
+      <option v-for="i in users" v-bind:key='i' v-bind:value='i'  >{{i.email}}</option>
+    </select>
+    <br>
+    <form @submit="submit1()" onsubmit = "return false">
+      <button type="submit">Ember hozzáadása</button>
+    </form>
+
       <h2>Törölj egy csoporttagot!</h2>
       <br>
-    <select name="" id="" multiple>
+    <select v-model="selected" multiple>
       <option disabled value="">Válaszd ki, kit szeretnél törölni!</option>
-      <option>...</option>
-      <option><button>Delete</button></option>
-     </select>
-        <br>
+      <option v-for="i in members" v-bind:key='i' v-bind:value='i'  >{{i.email}}</option>
+    </select>
+    <br>
+    <form @submit="submit2()" onsubmit = "return false">
+      <button type="submit">Ember törlése</button>
+    </form>
+
     <h2>Adj másnak is admmin jogot!</h2>
     <br>
     <select name="" id="" multiple>
       <option disabled value="">Válaszd ki, kinek akarsz admin jogot adni!</option>
        <option>...</option>
-       <option><button>Submit</button></option>
     </select>
+        <form @submit="submit3()" onsubmit = "return false">
+      <button type="submit">Felruházás admin joggal</button>
+    </form>
+    <br>
+    <h2>AVedd el valaki admin jogát!</h2>
+    <br>
+    <select name="" id="" multiple>
+      <option disabled value="">Válaszd ki, kinek az admin jogát akarod elvenni!</option>
+      <option>...</option>
+    </select>
+    <form @submit="submit4()" onsubmit = "return false">
+      <button type="submit">Admin jog elvétele</button>
+    </form>
 </div>
 </template>
 
@@ -43,30 +61,44 @@ export default {
 
   computed: {
     ...mapState(['token']),
-    ...mapState('modGroup', ['group', 'users'])
+    ...mapState('modGroup', ['group', 'users', 'members', 'admins'])
   },
 
   methods: {
-    ...mapActions('modGroup', ['getGroup', 'getUsers']),
-    submit () {
+    ...mapActions('modGroup', ['getGroup', 'getUsers', 'addMembers', 'delMembers', 'addAdmin', 'delAdmin']),
+    submit1 () {
+      this.addMembers({ token: this.token, memberList: this.selected })
+    },
+    submit2 () {
+      this.delMembers({ token: this.token, memberList: this.selected })
+    },
+    submit3 () {
+      this.addAdmin({ token: this.token, adminList: this.selected })
+    },
+    submit4 () {
+      this.delAdmin({ token: this.token, adminList: this.selected })
     }
   },
 
   data () {
     return {
-      selected: '',
-      select: ''
+      selected: []
+      // select: ''
     }
   },
   created () {
     this.getGroup({ token: this.token })
     this.getUsers({ token: this.token })
+    this.addMembers({ token: this.token })
+    this.delMembers({ token: this.token })
+    this.addAdmin({ token: this.token })
+    this.delAdmin({ token: this.token })
   },
 
   watch: {
     selected () {
       this.getGroup({ id: this.selected, token: this.token })
-      this.getUsers({ id: this.selected, token: this.token })
+      // this.getUsers({ id: this.selected, token: this.token })
     }
   }
 }
--- frontend/anya-elmentem/src/views/modGroup/vuex/actions.js
+++ frontend/anya-elmentem/src/views/modGroup/vuex/actions.js
@@ -3,7 +3,6 @@ import apiUser from '../../../api/user-api'
 
 export default {
   getGroup ({ commit }, params) {
-    console.log(params)
     apiGroup.getGroup(params).then(response => {
       commit('setGroup', response.data)
     }).catch(error => {
@@ -13,6 +12,34 @@ export default {
   getUsers ({ commit }, params) {
     apiUser.getUsers(params).then(response => {
       commit('setUsers', response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  addMembers ({ commit }, params) {
+    apiGroup.addMembers(params).then(response => {
+      commit('addMembers', response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  delMembers ({ commit }, params) {
+    apiGroup.delMembers(params).then(response => {
+      commit('delMembers', response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  addAdmin ({ commit }, params) {
+    apiGroup.addAdmin(params).then(response => {
+      commit('addAdmin', response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  },
+  delAdmin ({ commit }, params) {
+    apiGroup.delAdmin(params).then(response => {
+      commit('delAdmin', response.data)
     }).catch(error => {
       console.log(error)
     })
