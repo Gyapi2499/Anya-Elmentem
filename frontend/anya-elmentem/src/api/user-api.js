08091dad79d509863c3c26aa5b15d5446da65b6a
Gyapi2499 <g70r8p@inf.elte.hu>
2020-05-19 01:22:53 +0200

    Update user-api.js

--- frontend/anya-elmentem/src/api/user-api.js
+++ frontend/anya-elmentem/src/api/user-api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   register (userRequest) {
-    return axios.post('http://localhost:8080/users/users/register', {
+    return axios.post('https://my-app-no.herokuapp.com/users/register', {
       username: userRequest.name,
       email: userRequest.email,
       password: userRequest.password
